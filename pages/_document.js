813f5cf738b20c6dee4536907bd24da845fa5a27
Oli <oliverrsaxon@gmail.com>
2022-07-01 19:18:17 +0100

    Reval

--- pages/_document.js
+++ pages/_document.js
@@ -4,7 +4,14 @@ export default class MyDocument extends Document {
 	render() {
 		return (
 			<Html lang="en">
-				<Head />
+				<Head>
+					<link
+						rel="preload"
+						href="/fonts/Rubik-80sFade-Regular.ttf"
+						as="font"
+						crossOrigin=""
+					/>
+				</Head>
 				<body className="dark:bg-slate-900 bg-slate-50">
 					<Main />
 					<NextScript />
